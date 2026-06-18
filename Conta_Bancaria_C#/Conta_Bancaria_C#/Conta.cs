54e4f21ce07dd3a19f816d3949f29d52201bef2d
Gustavo Dos Santos <gustavo.demagsp@gmail.com>
2026-06-18 15:05:08 -0300

    feat: Classe Conta, Encapsulamento e Auto Properties

---
+++ Conta_Bancaria_C#/Conta_Bancaria_C#/Conta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Conta_Bancaria
+{
+    internal class Conta
+    {
+        private int NumeroConta;
+        public string Titular { get; private set; } //autoproperty para o titular da conta, permitindo apenas leitura fora da classe
+        public double Saldo { get; private set; } //autoproperty para o saldo da conta, permitindo apenas leitura fora da classe
+
+        public Conta(int numeroConta, string titular, double saldo) // Construtor para inicializar os atributos da conta com saldo inicial 
+        {
+            NumeroConta = numeroConta;
+            Titular = titular;
+            Saldo = saldo;
+        }
+        public Conta(int numeroConta, string titular) : this(numeroConta, titular, 0.0) // Sobrecarga do construtor para permitir a criação de uma conta sem depósito inicial caso não tenha depósito inicial
+        {
+            Saldo = 0.0;
+        }
+        public void Deposito(double Valor)
+        {
+            Saldo += Valor;
+        }
+
+        public void Saque(double Valor)
+        {
+            Saldo -= Valor + 5.00; // Método para realizar um saque, descontando uma taxa fixa de 5.00
+        }
+
+        public override string ToString()
+        {
+            return "Conta:" + NumeroConta +
+                   " Titular:" + Titular +
+                   " Saldo:" + Saldo.ToString("F2", CultureInfo.InvariantCulture); // Método para retornar uma string formatada com os detalhes da conta
+        }
+    }
+}
---
+++ Conta_Bancaria_C#/Conta_Bancaria_C#/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta_Bancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double _valor = 0.0;
+            int _numeroConta;
+            string _titular;
+
+            Console.Write("Entre o número da conta: ");
+            _numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            _titular = Console.ReadLine();
+
+            Conta C = new Conta(_numeroConta, _titular, _valor);
+
+            Console.Write("Haverá depósito inicial?(s/n): ");
+            char opcao = Convert.ToChar(Console.ReadLine());
+            if (opcao == 'S' || opcao == 's')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                _valor = double.Parse(Console.ReadLine()); //saldo inicial
+                C.Deposito(_valor);
+                C = new Conta(_numeroConta, _titular, _valor);
+            }
+            else
+            {
+                C = new Conta(_numeroConta, _titular);
+            }
+            Console.WriteLine(C);
+
+            Console.WriteLine("\nEntre o valor para depósito: ");
+            _valor = double.Parse(Console.ReadLine());
+            C.Deposito(_valor);
+
+            Console.Write("Dados atualizados: " + C);
+
+            Console.Write("\nEntre o valor para saque: ");
+            _valor = double.Parse(Console.ReadLine());
+            C.Saque(_valor);
+            Console.WriteLine("Dados atualizados: " + C);
+        }
+    }
+}
+
+
