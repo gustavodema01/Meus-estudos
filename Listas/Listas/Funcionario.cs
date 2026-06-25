91b9c3955ae19d09c7ca99f8c5155b3a07fcebb2
Gustavo Dos Santos <gustavo.demagsp@gmail.com>
2026-06-25 15:15:57 -0300

    implementa lista de funcionários com aumento de salário

---
+++ Listas/Listas/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+
+namespace Listas
+{
+    internal class Funcionario
+    {
+        public int ID { get; private set; }
+        public string Nome { get; private set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            ID = id;
+            Nome = nome;
+            Salario = salario;
+        }
+        public void AumentarSalario(double percentual)
+        {
+            Salario += Salario * percentual / 100.0;
+        }
+
+        public override string ToString()
+        {
+           return "ID: " + ID +
+                "\nNome: " + Nome +
+                "\nSalario: " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 0;
+            string name = "";
+            double salario = 0.0;
+
+            Console.Write("How many employees will be registered?");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Funcionario> list = new List<Funcionario>(); //lista que só aceita instaciações de Funcionario
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Emplyoee #" + (i + 1));
+                Console.Write("ID: ");
+                id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+                Console.Write("Salary: ");
+                salario = int.Parse(Console.ReadLine());
+                Funcionario funcionario = new Funcionario(id, name, salario); //instanciação de um objeto do tipo Funcionario
+                list.Add(funcionario); //adiciona o objeto funcionario na lista,
+            }
+
+            Console.WriteLine("\nUpdated list of employees: ");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+                Console.WriteLine("");  
+            }
+            bool validacao = false;
+            Funcionario funcionarioencontrado = null;
+            while (!validacao)
+            {
+                Console.Write("Enter the employee id that will have salary increase :");
+                int idapoio = int.Parse(Console.ReadLine());
+                for(int i = 0; i < list.Count; i++) 
+                {
+                    if (idapoio == list[i].ID)
+                    {
+                        funcionarioencontrado = list[i]; // guarda a referência
+                        validacao = true; 
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("This id does not exist!");
+                        validacao = false; //sai do while
+                        break;
+                    }
+                }
+            }
+            
+            Console.Write("Enter the percentage:");
+            double porcentagem = double.Parse(Console.ReadLine());
+
+            funcionarioencontrado.AumentarSalario(porcentagem);
+
+            Console.WriteLine("\nUpdated list of employees: ");
+            foreach(Funcionario obj in list) //foreach no lugar do for para percorrer a lista de funcionários
+            {
+                Console.WriteLine(obj);
+            }
+        }
+
+    }
+}
+
+
