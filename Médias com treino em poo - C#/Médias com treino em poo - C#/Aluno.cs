abb2e6ef1a0c2b1e7554052c5d5390682dc6bcd6
Gustavo Dos Santos <gustavo.demagsp@gmail.com>
2026-06-28 18:44:55 -0300

    exercício de fixação - cadastro de alunos com médias em C#

---
+++ Médias com treino em poo - C#/Médias com treino em poo - C#/Aluno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Médias_Escolares
+{
+    internal class Aluno
+    {
+        public string Name { get; private set; }
+        public int Enrollment { get; private set; }
+        public double Grade { get; private set; }
+
+        public Aluno(string name, int enrollment, double grade) //colocar validação aqui
+        {
+            Name = name;
+            Enrollment = enrollment;
+            Grade = grade;
+        }
+        public string CalcSituation()
+        {
+            return Grade>=5 ?  "Approved!" : "Failed"; //ternary condition to check if the student is approved or not      
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Enrollment: {Enrollment}, Situation : {CalcSituation()}";
+        }
+
+    }
+}
---
+++ Médias com treino em poo - C#/Médias com treino em poo - C#/Program.cs
@@ -0,0 +1,66 @@
+﻿using Médias_com_treino_em_poo___C_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Médias_Escolares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Aluno> list = new List<Aluno>();
+
+            bool validacao = false;
+            while (!validacao)
+            {
+                Console.Clear();
+                Console.WriteLine("1. STUDENT REGISTER");
+                Console.WriteLine("2. STUDENT SITUATION");
+                Console.WriteLine("3. STUDENT SEARCHING");
+                Console.WriteLine("4. QUIT");
+                Console.Write("\n Wich option?: ");
+                int opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.Write("STUDENT NAME: ");
+                        string name = Console.ReadLine();  //colocar validação aqui
+                        Console.Write("STUDENT ENROLLMENT: ");
+                        int enrollment = int.Parse(Console.ReadLine());  //colocar validação aqui
+                        Console.Write("STUDENTE GRADE: "); 
+                        double grade = int.Parse(Console.ReadLine());
+                        Aluno student = new Aluno(name, enrollment, grade); // create a new instance of Aluno
+                        list.Add(student); // list with objects of student type
+                        validacao  = false;   
+                    break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.Write("STUDENT ENROLLMENT: ");
+                        Aluno found = list.Find(x => x.Enrollment == int.Parse(Console.ReadLine())); //colocar validação aqui 
+                        Console.WriteLine(found.CalcSituation());
+                        Console.ReadKey();
+                        validacao = false;
+                        break;
+
+                        case 3:
+                        Console.Clear();
+                        Console.Write("Write the student's ID number: ");
+                        Aluno found2 = list.Find(x => x.Enrollment == int.Parse(Console.ReadLine())); //colocar validação aqui
+                        Console.WriteLine(found2);
+                        Console.ReadKey();
+                        break;
+
+                        case 4:
+                        validacao = false;
+                        break;
+                }
+            }
+        }
+    }
+}
